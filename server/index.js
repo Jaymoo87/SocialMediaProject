db0791cab3e74e4d6022e82a57391fe75ae6c69f
Justin Murrah <jdmurrah@gmail.com>
2023-02-08 22:09:15 -0600

    social media app project

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postsRouter from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postsRouter);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://Jaymoo:Rowan2014@cluster0.fhtxyv6.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on: ${PORT}`)))
+  .catch((error) => console.log(error.message));
