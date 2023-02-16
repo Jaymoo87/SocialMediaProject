8fe6046bb2ec1f8c61b8b288c0aece4f903a3ee8
Justin Murrah <jdmurrah@gmail.com>
2023-02-16 15:00:07 -0600

    Navigate instead of Redirect is the way to go

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Container } from "@material-ui/core";
 
 import NavBar from "./components/navbar/NavBar.js";
 import Home from "./components/Home/Home.js";
 import Auth from "./components/auth/Auth.js";
+import PostDetails from "./components/postDetails/PostDetails.jsx";
 
 const App = () => {
   return (
@@ -13,9 +14,10 @@ const App = () => {
       <Container maxWidth="xl">
         <NavBar />
         <Routes>
-          <Route path="/" element={() => <Redirect to="/posts" />}></Route>
+          <Route path="/" element={<Navigate to="/posts" />}></Route>
           <Route path="/posts" element={<Home />}></Route>
           <Route path="/posts/search" element={<Home />}></Route>
+          <Route path="/posts/:id" element={<PostDetails />}></Route>
           <Route path="/auth" element={<Auth />}></Route>
         </Routes>
       </Container>
