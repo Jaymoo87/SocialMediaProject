8024e704e5de50ef95b1ad4db48a90d961d8a2a9
Justin Murrah <jdmurrah@gmail.com>
2023-02-16 17:35:17 -0600

    added some search fields and started implemtenting search query logic

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import Auth from "./components/auth/Auth.js";
 import PostDetails from "./components/postDetails/PostDetails.jsx";
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem("profile"));
+
   return (
     <BrowserRouter>
       <Container maxWidth="xl">
@@ -18,7 +20,7 @@ const App = () => {
           <Route path="/posts" element={<Home />}></Route>
           <Route path="/posts/search" element={<Home />}></Route>
           <Route path="/posts/:id" element={<PostDetails />}></Route>
-          <Route path="/auth" element={<Auth />}></Route>
+          <Route path="/auth" element={!user ? <Auth /> : <Navigate to="/posts" />}></Route>
         </Routes>
       </Container>
     </BrowserRouter>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,29 +1,89 @@
 import React, { useEffect, useState } from "react";
 
-import { Container, Grow, Grid, Paper } from "@material-ui/core";
+import { Container, Grow, Grid, Paper, AppBar, Button, OutlinedInput, TextField } from "@material-ui/core";
 import { useDispatch } from "react-redux";
-import { getPosts } from "../../actions/posts";
+import { getPosts, getPostBySearch } from "../../actions/posts";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import Pagination from "../Pagination";
 import Posts from "../posts/Posts";
 import Form from "../form/Form";
+import useStyles from "./styles";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(null);
+  const [search, setSearch] = useState("");
+  const [tags, setTags] = useState("");
 
   const dispatch = useDispatch();
+  const nav = useNavigate();
+  const classes = useStyles();
+  const query = useQuery();
+  const page = query.get("page") || 1;
+  const searchQuery = query.get("searchQuery");
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+    }
+  };
+
+  const searchPost = () => {
+    if (!search.trim()) {
+      dispatch(getPostBySearch({ search, tags: tags.join(",") }));
+    } else {
+      nav("/");
+    }
+  };
+
+  const handleAddTAG = (tag) => setTags([...tags, tag]);
+  const handleDeleteTAG = (TAGToDelete) => setTags(tags.filter((tag) => tag !== TAGToDelete));
 
   useEffect(() => {
     dispatch(getPosts());
   }, [dispatch, currentId]);
   return (
     <Grow in>
-      <Container>
-        <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-          <Grid item xs={12} sm={7}>
+      <Container maxWidth="xl">
+        <Grid
+          container
+          justifyContent="space-between"
+          alignItems="stretch"
+          spacing={3}
+          className={classes.gridContainer}
+        >
+          <Grid item xs={12} sm={6} m={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={6} m={3}>
+            <AppBar className={classes.appBarSearch} position="static" color="inherit">
+              <TextField
+                name="search"
+                type="search"
+                variant="outlined"
+                label="Search Memories"
+                // onKeyDown={handleKeyDown}
+                fullWidth
+                value="TEST"
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <TextField
+                type="search"
+                style={{ margin: "10px 0" }}
+                value={tags}
+                // onAdd={(tag) => handleAddTAG(tag)}
+                // onDelete={(tag) => handleDeleteTAG(tag)}
+                label="Search Tags"
+                variant="outlined"
+              />
+              <Button onClick={searchPost} className={classes.searchButton} color="primary">
+                {" "}
+                Search{" "}
+              </Button>
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
             <Paper elevation={6}>
               <Pagination />
