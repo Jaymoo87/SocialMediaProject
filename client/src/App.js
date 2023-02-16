07b4f9324c90db5f00ecab531f6974f6efd4bcf5
Justin Murrah <jdmurrah@gmail.com>
2023-02-16 13:42:28 -0600

    started pagination, gotta check Redirect with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
 import { Container } from "@material-ui/core";
 
 import NavBar from "./components/navbar/NavBar.js";
@@ -10,10 +10,12 @@ import Auth from "./components/auth/Auth.js";
 const App = () => {
   return (
     <BrowserRouter>
-      <Container maxWidth="lg">
+      <Container maxWidth="xl">
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={() => <Redirect to="/posts" />}></Route>
+          <Route path="/posts" element={<Home />}></Route>
+          <Route path="/posts/search" element={<Home />}></Route>
           <Route path="/auth" element={<Auth />}></Route>
         </Routes>
       </Container>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 
-import { Container, Grow, Grid } from "@material-ui/core";
+import { Container, Grow, Grid, Paper } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import { getPosts } from "../../actions/posts";
 
+import Pagination from "../Pagination";
 import Posts from "../posts/Posts";
 import Form from "../form/Form";
 
@@ -24,6 +25,9 @@ const Home = () => {
           </Grid>
           <Grid item xs={12} sm={4}>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper elevation={6}>
+              <Pagination />
+            </Paper>
           </Grid>
         </Grid>
       </Container>
